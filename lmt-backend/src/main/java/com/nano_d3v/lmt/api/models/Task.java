4fc4eefec2f2b816c2268e76c3dfa4f84e5ee8ba
MELZI RAYANE p2512608 <rayane.melzi@etu.univ-lyon1.fr>
2026-07-04 06:12:07 +0200

    map task enum values to match frontend json format

--- lmt-backend/src/main/java/com/nano_d3v/lmt/api/models/Task.java
+++ lmt-backend/src/main/java/com/nano_d3v/lmt/api/models/Task.java
@@ -1,22 +1,27 @@
 package com.nano_d3v.lmt.api.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Task {
-   
+
 public enum Importance {
+    @JsonProperty("low")
     LOW,
-    HIGH    
+    @JsonProperty("high")
+    HIGH
 }
 public enum Urgency{
-    urgent,
-    not_urgent
+    @JsonProperty("urgent")
+    URGENT,
+    @JsonProperty("not urgent")
+    NOT_URGENT
 }
     
     //A task has an id, content, importance, urgency, columnId
